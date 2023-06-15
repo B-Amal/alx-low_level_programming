1def784f20539c83e16d56662c169fb503f90c18
B-Amal <amalboughzala1@gmail.com>
2023-06-15 16:26:16 -0700

    Add script of task: UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - The program prints a string followed by \n to the standard error.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
