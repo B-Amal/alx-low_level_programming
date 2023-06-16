81b7458bb38a4f07c47e5bb3c6ee29b1549504fd
B-Amal <amalboughzala1@gmail.com>
2023-06-16 15:29:49 -0700

    Add script of task: Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The program starts with 0, prints all single digit num of base 10.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	printf("%d",i);
+	putchar('\n');
+
+	return (0);
+}
