a7e13f67cf6a3ad4546126c15e2d37f4bcb604a3
B-Amal <amalboughzala1@gmail.com>
2023-07-17 12:40:06 -0700

    Task-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - creates a struct dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
