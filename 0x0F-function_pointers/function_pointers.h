6fb299e24bbe92dd4f92d6c3ab7dcee98d5dd07e
B-Amal <amalboughzala1@gmail.com>
2023-07-19 06:16:50 -0700

    Remove some files

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,8 +0,0 @@
-#include <stddef.h>
-#ifndef FUNC_POINTERS_H
-#define FUNC_POINTERS_H
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-
-#endif
