87c706212ea9d86c4fb6a8e7481175e4468cc0be
B-Amal <amalboughzala1@gmail.com>
2023-09-14 14:55:02 -0700

    Task-doublylinkedlists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - func
+ * @head: head
+ * Return: the sum of all the data (n) of a dlistint_t linked list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
