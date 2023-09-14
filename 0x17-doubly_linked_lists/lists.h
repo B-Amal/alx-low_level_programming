ab075073ee4c5c0f85b823ffae65c4d1678ee056
B-Amal <amalboughzala1@gmail.com>
2023-09-14 11:45:07 -0700

    Task-doublylinkedlists

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * struct dlistint_st - doubly linked list node structure
+ * @n: val
+ * @prev: previous node
+ * @next: next node
+ */
+typedef struct dlistint_st
+{
+	int n;
+	struct dlistint_st *prev;
+	struct dlistint_st *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
